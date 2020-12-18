1a175b55112f275ce4e7e506ac45288f1be2d24e
Tyler C Smith <tylercsmith94@gmail.com>
2020-12-19 01:52:58 +0700

    Styles die component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Die />
+      <Die face="five" />
+      <Die face="six" />
+      <Die face="three" />
+      <Die face="two" />
     </div>
   );
 }
--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react'
 import React, { Component } from 'react'
+import './Die.css';
 
 
 class Die extends Component{
   render(){
-    return <i className="fas fa-dice-one"></i>
+    return <i className={`Die fas fa-dice-${this.props.face}`}></i>
   }
 }
 
