719dc8bfe6f4dba33f5eabf0357a1576be6ff5a5
Tyler C Smith <tylercsmith94@gmail.com>
2020-12-19 01:49:56 +0700

    Create Die Component, Adds fontawesome cdn

---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+
+
+class Die extends Component{
+  render(){
+    return <i className="fas fa-dice-one"></i>
+  }
+}
+
+export default Die;
