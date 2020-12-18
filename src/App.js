745af80db2074fe87eef174a5002c00365fcf14e
Tyler C Smith <tylercsmith94@gmail.com>
2020-12-19 01:56:10 +0700

    Adds RollDice Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import Die from './Die'
+import RollDice from './RollDice'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Die face="five" />
-      <Die face="six" />
-      <Die face="three" />
-      <Die face="two" />
+      <RollDice />
     </div>
   );
 }
